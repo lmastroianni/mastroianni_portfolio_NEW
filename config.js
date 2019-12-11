e194d329b23313d2f3bd4f661fe001ef5cf2e845
lmastroianni <43182996+lmastroianni@users.noreply.github.com>
2019-12-11 12:28:10 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const config = {
-    host: "localhost",
+    host: "us-cdbr-iron-east-05.cleardb.net",
     port: 3306, // 3306 for windows/linux or newer macs
-    uname: "root",
-    pword: "", // blank for windows
-    dbase: "db_final"
+    uname: "b4c67c91acbf76",
+    pword: "0ab0c5cb", // blank for windows
+    dbase: "heroku_81eccc719568191"
 }
 
 module.exports = config;
