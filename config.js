9e6d659477e6dca8d90700e6301b8be3f53e8896
Lucas Mastroianni <lcmhockey@hotmail.com>
2019-12-11 18:57:25 -0500

    heroku port info and more css

--- config.js
+++ config.js
@@ -7,3 +7,14 @@ const config = {
 }
 
 module.exports = config;
+
+  
+// const config = {
+//     host: "us-cdbr-iron-east-05.cleardb.net",
+//     // port: 3306, // 3306 for windows/linux or newer macs
+//     uname: "b4c67c91acbf76",
+//     pword: "0ab0c5cb", // blank for windows
+//     dbase: "heroku_81eccc719568191"
+// }
+
+// module.exports = config;
