c15f81af9519f143fd7f94c023483df4f61e67c4
Lucas <lcmhockey@hotmail.com>
2019-12-15 14:36:49 -0500

    final push

--- config.js
+++ config.js
@@ -1,22 +1,22 @@
-const config = {
-    host: "us-cdbr-iron-east-05.cleardb.net",
-    // port: 3306, // 3306 for windows/linux or newer macs
-    uname: "b4c67c91acbf76",
-    pword: "0ab0c5cb", // blank for windows
-    dbase: "heroku_81eccc719568191"
-}
-
-module.exports = config;
-
 // const config = {
-//     host: "localhost",
-//     port: 8889, // 3306 for windows/linux or newer macs
-//     uname: "root",
-//     pword: "root", // blank for windows
-//     dbase: "db_final"
+//     host: "us-cdbr-iron-east-05.cleardb.net",
+//     // port: 3306, // 3306 for windows/linux or newer macs
+//     uname: "b4c67c91acbf76",
+//     pword: "0ab0c5cb", // blank for windows
+//     dbase: "heroku_81eccc719568191"
 // }
 
 // module.exports = config;
+
+const config = {
+    host: "localhost",
+    port: 8889, // 3306 for windows/linux or newer macs
+    uname: "root",
+    pword: "root", // blank for windows
+    dbase: "db_final"
+}
+
+module.exports = config;
 
 
 
